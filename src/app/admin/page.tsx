30f36b755024ace8205aa0a9eadbb7f11e2bef57
Randy Kim <rhkim1292@gmail.com>
2024-05-25 17:36:10 -0700

    Setup prisma to allow for database interaction
    Currency and number formatter also added.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,26 +5,41 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import db from "@/db/db";
 
-function getSalesData() {
-    
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
 }
 
-export default function AdminDashboard() {
+export default async function AdminDashboard() {
+  const salesData = await getSalesData();
+
   return (
     <div
       className="grid grid-cols-1 md:grid-cols-2 
     lg:grid-cols-3 gap-4"
     >
-      <DashboardCard title="Sales" subtitle="Test" body="body" />
+      <DashboardCard
+        title="Sales"
+        subtitle={salesData.numberOfSales}
+        body={salesData.amount}
+      />
     </div>
   );
 }
 
 type DashboardCardProps = {
   title: string;
-  subtitle: string;
-  body: string;
+  subtitle: number;
+  body: number;
 };
 
 function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
