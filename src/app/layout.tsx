8a7c47409e259d814c713b9695992fdf6f7d80d0
Randy Kim <rhkim1292@gmail.com>
2024-06-01 22:29:58 -0700

    Write a few comments in utils.ts and layout.tsx
    The layout.tsx is the layout file for the root app route.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
+// The Inter function is used to configure the application's font and its
+//options, such as the subsets and variable name and then store the return
+//value in the inter variable, which makes it available for use in the app.
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
@@ -10,6 +13,13 @@ export const metadata: Metadata = {
 	description: "The go-to shop for all your newborn's needs.",
 };
 
+/**
+ * Renders the root layout of the application.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {React.ReactNode} props.children - The content to be rendered within the layout.
+ * @return {JSX.Element} The root layout component.
+ */
 export default function RootLayout({
 	children,
 }: Readonly<{
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Merges multiple class values into a single string using the `clsx` and `twMerge` functions.
+ *
+ * @param {...ClassValue[]} inputs - The class values to be merged.
+ * @return {string} The merged class value string.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
