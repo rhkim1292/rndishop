7a91245bf7beeb992d5872eee3f5a8e7b0062cd2
Randy Kim <rhkim1292@gmail.com>
2024-06-10 21:35:46 -0700

    Force purchase success page to be dynamic

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -8,6 +8,8 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
+export const dynamic = "force-dynamic";
+
 export default async function SuccessPage({
 	searchParams,
 }: {
